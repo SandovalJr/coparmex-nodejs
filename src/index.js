347a97fe7f0f2db944b9950c36846b33eb0253a3
Eduardo Sandoval <edubizzle7@gmail.com>
2021-08-12 20:43:17 -0500

    algunas cositas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-const express = require ('express')
-const morgan = require ('morgan')
 
-const app = express()
-
-app.use(morgan('dev'))
-
-app.listen(3000)
-console.log('Server on port', 3000);
+const app = require('./app')
+app.listen(app.get('port'))
+console.log('Server on port', app.get('port'));
 
---
+++ src/routes/employees.routes.js
@@ -0,0 +1,7 @@
+const {Router} = require('express')
+const router = Router()
+
+//Aqui escribiremos la rutas que necesitemos
+
+
+module.exports = router
